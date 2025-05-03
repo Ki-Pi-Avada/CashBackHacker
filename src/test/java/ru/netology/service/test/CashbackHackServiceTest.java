b6d4f5d74d12e802539b0331da352bc9d56ddeda
Kifo <i.g.a.n.garsia@gmail.com>
2025-05-03 17:57:57 +0300

    testNG

---
+++ src/test/java/ru/netology/service/test/CashbackHackServiceTest.java
@@ -0,0 +1,51 @@
+package ru.netology.service.test;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldReturnRemainForAmountLessThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldReturnZeroWhenAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnRemainForAmountMoreThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int expected = 900;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnRemainForAmountTwiceBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnBoundaryForZeroAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+}
